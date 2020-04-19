066c55ebdb5ddadc3ff46a44a5f67356f151e963
Jinwook Shin <7inug1@gmail.com>
2020-04-19 12:28:11 -0700

    final changes
    revisited the code to organize better and make documentation.

--- AppleDropperProject/appledropper.js
+++ AppleDropperProject/appledropper.js
@@ -1,19 +1,35 @@
-//Things to remember :
-//1. window.onload = redraw;
-//2. event.key=="ArrowLeft"
-//3. ctx.drawImage(img, clipStartX, clipStartY, clipEndX, clipEndY, XonCanvas, YonCanvas, imgWidthX, imgWidthY)
-//ctx.drawImage(doraemon,0,0,200,200,doraemonX,300,100,100);
-
+// -Name : Jinwook Shin
+//
+// -From : Langara College, Computer Studies
+//
+// -Last Date Modified : 4/19/2020 (Sun)
+//
+// -Instruction : A revision from the Apple Dropper Game. Use the buttons provided
+// or arrowleft and arrowright keyboards to move the character around to catch the
+// food dropping from the top. If you achieve the designated score, you win the game.
+//
+// -(Personal) Note :
+// Things to remember :
+// 1. window.onload = redraw;
+// 2. event.key=="ArrowLeft"
+// 3. ctx.drawImage(img, clipStartX, clipStartY, clipEndX, clipEndY, XonCanvas, YonCanvas, imgWidthX, imgWidthY)
+// ctx.drawImage(doraemon,0,0,200,200,doraemonX,300,100,100);
+//
+// Canvas size :
 // width:600
 // height:400
+
+//Using canvas
 let canvas=document.getElementById("myCanvas");
 let ctx=canvas.getContext("2d");
 //------------------------------------------------
+//Global variables
 let score=0;
+let goal=5;
 let timer;
 let time;
-let goal=5;
 //------------------------------------------------
+//Calling image to use
 let background=new Image();
 background.src="room.jpg";
 let doraemon=new Image();//basket
@@ -21,12 +37,13 @@ doraemon.src="doraemon.png";
 let dorayaki=new Image();//apple
 dorayaki.src='dorayaki.png';
 //------------------------------------------------
-let dora=new Doraemon(-25, 300);
-let yaki=new Dorayaki((Math.floor)(Math.random()*556), -15);
-window.onload = redraw;
-document.addEventListener("keydown", press);
+let dora=new Doraemon(-25, 300); //doraemon's starting position
+let yaki=new Dorayaki((Math.floor)(Math.random()*556), -15); //yaki being called at a random position at the top
+window.onload = redraw; //Upon loading the page, you redraw
+document.addEventListener("keydown", press); //can move doraemon using arrowleft and arrowright keyboard
 displayGoal();
 //------------------------------------------------
+//Constructors
 //basket
 function Doraemon(x, y){
   this.x=x;
@@ -37,7 +54,8 @@ function Dorayaki(x, y){
   this.x=x;
   this.y=y;
 }
-
+//------------------------------------------------
+//Drawing on page
 function drawBackground(){
   ctx.clearRect(0,0,600,400);
   ctx.drawImage(background, 0, 0, 600, 400);
@@ -62,7 +80,8 @@ window.onload = function() {
   drawDoraemon();
   drawDorayaki();
 }
-
+//------------------------------------------------
+//Move doraemon's directions
 function moveLeft(){
   if(dora.x>(-25))
     dora.x-=25;
@@ -82,35 +101,22 @@ function press(){
     moveRight();
   }
 }
+//------------------------------------------------
 
 function checkCollision(){
   //if(yakileft vs left / yakileft vs right) || yakiright vs left / yakiright vs right
-  if(    (yaki.x-30>=dora.x)&&(yaki.x<=dora.x+80) ||  (yaki.x+45-30>=dora.x)&&(yaki.x+45<=dora.x+100)){
+  if( (yaki.x-30>=dora.x)&&(yaki.x<=dora.x+80) || (yaki.x+45-30>=dora.x)&&(yaki.x+45<=dora.x+100) ){
     return true;
   }else{
     return false;
   }
 }
-
-function executeProgram(){
-  output.innerHTML=score;
-  yaki.y+=10;
-  redraw();
-  if(yaki.y>400){
-    yaki.x=(Math.floor)(Math.random()*556);
-    yaki.y=0;
-  }else if(yaki.y+50>dora.y && checkCollision()==true){
-    score+=1;
-    output.innerHTML=score;
-    yaki.x=(Math.floor)(Math.random()*556);
-    yaki.y=0;
-  }
-}
-
+//------------------------------------------------
+//Timer-related functions
 function startTimer(){
   time=0;//convert a variable to a number
   if(timer==undefined){
-      timer=setInterval(count, 100);
+      timer=setInterval(count, 100); //interval by 0.1 seconds || 100 milliseconds
   }
 }
 
@@ -131,6 +137,25 @@ function stopTimer(){
 
 function reset(){
   score=0;
+}
+//------------------------------------------------
+//running the function by each interval.
+function executeProgram(){
+  output.innerHTML=score;
+  yaki.y+=10;
+  redraw();
+  //yaki not being caught into the basket (earning no score)
+  if(yaki.y>400){
+    yaki.x=(Math.floor)(Math.random()*556);
+    yaki.y=0;
+  }
+  //yaki being caught into the basket (earning score)
+  else if(yaki.y+50>dora.y && checkCollision()==true){
+    score+=1;
+    output.innerHTML=score;
+    yaki.x=(Math.floor)(Math.random()*556);
+    yaki.y=0;
+  }
 }
 
 function displayGoal(){
