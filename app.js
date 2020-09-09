a4d76735335f8dbb4110409b7f6e05d31dd4cc07
Jinwook Shin <7inug1@gmail.com>
2020-09-09 11:03:33 -0700

    changes

---
+++ app.js
@@ -0,0 +1,19 @@
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('./sw.js')
+  .then(function(register) {
+    if(register.installing) {
+      console.log('Service worker installing');
+    } else if(register.waiting) {
+      console.log('Service worker installed');
+    } else if(register.active) {
+      console.log('Service worker active');
+    }
+    console.log('Registration successful, scope is:', registration.scope);
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
--- sw.js
+++ sw.js
@@ -9,21 +9,21 @@
 // https://stackoverflow.com/questions/34160509/options-for-testing-service-workers-via-http
 // https://gist.github.com/Rich-Harris/fd6c3c73e6e707e312d7c5d7d0f3b2f9
 
-if ('serviceWorker' in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using the default scope.
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' })
-  .then(function(register) {
-    if(register.installing) {
-      console.log('Service worker installing');
-    } else if(register.waiting) {
-      console.log('Service worker installed');
-    } else if(register.active) {
-      console.log('Service worker active');
-    }
+// if ('serviceWorker' in navigator) {
+//   // Register a service worker hosted at the root of the
+//   // site using the default scope.
+//   navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' })
+//   .then(function(register) {
+//     if(register.installing) {
+//       console.log('Service worker installing');
+//     } else if(register.waiting) {
+//       console.log('Service worker installed');
+//     } else if(register.active) {
+//       console.log('Service worker active');
+//     }
 
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-}
+//   }).catch(function(error) {
+//     // registration failed
+//     console.log('Registration failed with ' + error);
+//   });
+// }
