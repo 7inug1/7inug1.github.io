ae2de68452d1a6db15234978c3871615ea9e02a5
Jinwook Shin <7inug1@gmail.com>
2020-09-10 12:38:12 -0700

    Update sw.js

--- htmlProject/sw.js
+++ htmlProject/sw.js
@@ -8,17 +8,27 @@ self.addEventListener('install', function(event) {
       // takes an array of origin-relative URLs 
       // to all the resources you want to cache.
       return cache.addAll([
-        './index.html',
-        './7inug1.github.io/',
-        './7inug1.github.io/index.html',
-        './7inug1.github.io/letter.html',
-        './7inug1.github.io/style.css',
-        './7inug1.github.io/portfolio.css',
-        './7inug1.github.io/app.js',
-        './7inug1.github.io/canvas.js',
-        './7inug1.github.io/camera.js',
-        './7inug1.github.io/sw.js',
-        './7inug1.github.io/onlineOffline.js'
+        '/7inug1.github.io/',
+        '/7inug1.github.io/htmlProject/app.js',
+        '/7inug1.github.io/htmlProject/camera.js',
+        '/7inug1.github.io/htmlProject/canvas.js',
+        '/7inug1.github.io/htmlProject/letter.html',
+        '/7inug1.github.io/htmlProject/myLittleVader.jpg',
+        '/7inug1.github.io/htmlProject/onlineOffline.js',
+        '/7inug1.github.io/htmlProject/style.css',
+        '/7inug1.github.io/sw.js'
+
+
+        
+        // '/7inug1.github.io/index.html',
+        // '/7inug1.github.io/letter.html',
+        // '/7inug1.github.io/style.css',
+        // '/7inug1.github.io/portfolio.css',
+        // '/7inug1.github.io/app.js',
+        // '/7inug1.github.io/canvas.js',
+        // '/7inug1.github.io/camera.js',
+        // '/7inug1.github.io/sw.js',
+        // '/7inug1.github.io/onlineOffline.js'
       ]);
     })
   );
