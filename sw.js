e8758a71caf9a47089c445e57c23ebbf68533bd3
Jinwook Shin <7inug1@gmail.com>
2020-09-09 17:46:57 -0700

    temp changes

--- sw.js
+++ sw.js
@@ -33,3 +33,44 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+
+
+
+// With fetch event, you tell servie workers to do something
+// with the cached content. 
+self.addEventListener('fetch', function(event) {
+  // "caches.match(event.request)" allows us to match each
+  // resource requested from the network with the equivalent
+  // resource available in the cache, if there is a matching
+  // one available. 
+  
+  // call respondWith() on the event to hijack HTTP responses
+  // and update them with your own magic. 
+  event.respondWith(caches.match(event.request)
+  .then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } 
+    
+    else {
+      return fetch(event.request)
+      .then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1')
+        .then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/7inug1.github.io/myLittleVader.jpg');
+      });
+    }
+  }));
+});
