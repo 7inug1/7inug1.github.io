9d9196a40190893989e42bf3391ac3ddc7d98428
Jinwook Shin <7inug1@gmail.com>
2020-09-09 17:35:07 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
-  // https://github.com/mdn/sw-test
-  // https://mdn.github.io/sw-test/
+  // https://github.com/mdn/7inug1.github.io
+  // https://mdn.github.io/7inug1.github.io/
 // https://css-tricks.com/serviceworker-for-offline/
   // https://github.com/chriscoyier/Simple-Offline-Site
 // https://developers.google.com/web/fundamentals/primers/service-workers
@@ -9,21 +9,27 @@
 // https://stackoverflow.com/questions/34160509/options-for-testing-service-workers-via-http
 // https://gist.github.com/Rich-Harris/fd6c3c73e6e707e312d7c5d7d0f3b2f9
 
-// if ('serviceWorker' in navigator) {
-//   // Register a service worker hosted at the root of the
-//   // site using the default scope.
-//   navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' })
-//   .then(function(register) {
-//     if(register.installing) {
-//       console.log('Service worker installing');
-//     } else if(register.waiting) {
-//       console.log('Service worker installed');
-//     } else if(register.active) {
-//       console.log('Service worker active');
-//     }
+self.addEventListener('install', function(event) {
+  // this ensures that the service worker 
+  // will not install until the code inside 
+  // waitUntil() has successfully occurred.
 
-//   }).catch(function(error) {
-//     // registration failed
-//     console.log('Registration failed with ' + error);
-//   });
-// }
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      // takes an array of origin-relative URLs 
+      // to all the resources you want to cache.
+      return cache.addAll([
+        '/7inug1.github.io/',
+        '/7inug1.github.io/index.html',
+        '/7inug1.github.io/letter.html',
+        '/7inug1.github.io/style.css',
+        '/7inug1.github.io/portfolio.css',
+        '/7inug1.github.io/app.js',
+        '/7inug1.github.io/canvas.js',
+        '/7inug1.github.io/camera.js',
+        '/7inug1.github.io/sw.js',
+        '/7inug1.github.io/onlineOffline.js'
+      ]);
+    })
+  );
+});
