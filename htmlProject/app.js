ce961236668a2b0b452de27200f745de8e769965
Jinwook Shin <7inug1@gmail.com>
2020-09-10 12:39:42 -0700

    Update app.js

--- htmlProject/app.js
+++ htmlProject/app.js
@@ -5,6 +5,7 @@ if ('serviceWorker' in navigator) {
   // site using the default scope.
   navigator.serviceWorker.register('/htmlProject/sw.js', { scope: '/htmlProject/' })
   .then(function(register) {
+    console.log('Registration successful, scope is:', registration.scope);
     if(register.installing) {
       console.log('Service worker installing');
     } else if(register.waiting) {
@@ -12,7 +13,7 @@ if ('serviceWorker' in navigator) {
     } else if(register.active) {
       console.log('Service worker active');
     }
-    console.log('Registration successful, scope is:', registration.scope);
+    
 
   }).catch(function(error) {
     // registration failed
