8827aa1be1bcd38fca838bb3d14faba99a4f8d2b
Jinwook Shin <7inug1@gmail.com>
2020-09-11 09:53:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 // 7inug1.github.io
 
-// if ('serviceWorker' in navigator) {
-//   // Register a service worker hosted at the root of the
-//   // site using the default scope.
-//   navigator.serviceWorker.register('/htmlProject/sw.js', { scope: '/htmlProject/' })
-//   .then(function(register) {
-//     if(register.installing) {
-//       console.log('Service worker installing');
-//     } else if(register.waiting) {
-//       console.log('Service worker installed');
-//     } else if(register.active) {
-//       console.log('Service worker active');
-//     }
-//     console.log('Registration successful, scope is:' + registration.scope);
-
-//   }).catch(function(error) {
-//     // registration failed
-//     console.log('Registration failed with ' + error);
-//   });
-// }
-
-
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/ })
-    .then(registration => {
-      console.log('Service Worker is registered', registration);
-    })
-    .catch(err => {
-      console.error('Registration failed:', err);
-    });
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(register) {
+    if(register.installing) {
+      console.log('Service worker installing');
+    } else if(register.waiting) {
+      console.log('Service worker installed');
+    } else if(register.active) {
+      console.log('Service worker active');
+    }
+    console.log('Registration successful, scope is:' + registration.scope);
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
   });
 }
+
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js', { scope: '/ })
+//     .then(registration => {
+//       console.log('Service Worker is registered', registration);
+//     })
+//     .catch(err => {
+//       console.error('Registration failed:', err);
+//     });
+//   });
+// }
