c905119647404999513a233b8de95bf60a8301b1
Jinwook Shin <7inug1@gmail.com>
2020-09-11 09:28:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/htmlProject/sw.js')
+    navigator.serviceWorker.register('/sw.js')
     .then(registration => {
       console.log('Service Worker is registered', registration);
     })
