349925ac9bfc73765d5791098146d096929a4697
7inug1 <7inug1@gmail.com>
2021-01-06 08:08:57 +0900

    html5_project_update

---
+++ html5포폴_new_design/sw-test-newlyDeveloping/camera.js
@@ -0,0 +1,75 @@
+// <VARIABLES> - Related to web camera
+let video = document.getElementById('video');
+let streaming = false;
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+let constraints = (window.constraints = {
+  audio: false,
+  video: true,
+});
+
+// <VARIABLES> - Related to the canvas in 'Gallery' section
+let width = 320;
+let height = 240;
+
+// let canvasOne = document.getElementById('canvasOne');
+// let contextOne = canvasOne.getContext('2d');
+
+
+
+// <VARIABLES> - Buttons on web camera
+let cameraOnButton = document.getElementById('cameraOnButton');
+let cameraOffButton = document.getElementById('cameraOffButton');
+let cameraCaptureButton = document.getElementById('cameraCaptureButton');
+
+// <VARIABLES> - Related to 'local storage'
+// data: containing a representation of the image in the format specified by the type parameter (defaults to PNG). 
+let data;
+let key = "photoKey"; 
+
+console.log("camera")
+// <EVENTLISTENERS>
+cameraOnButton.addEventListener('click', turnCameraOn);
+cameraOffButton.addEventListener('click', turnCameraOff);
+cameraCaptureButton.addEventListener('click', captureImage);
+
+// <FUNCTIONS>
+function turnCameraOn(){
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function (error) {
+      console.log('Error has occured: ' + error);
+    });
+}
+
+// reference: https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/stop
+function turnCameraOff(){
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function (stream) {
+      video.srcObject = stream;
+      let tracks = stream.getTracks();
+      tracks.forEach(function(track) {
+        track.stop();
+      });
+      // srcObject is set to null to sever the link to the MediaStream object so it can be released.
+      video.srcObject = null;
+    })
+    .catch(function (error) {
+      console.log('getUserMedia() error', error);
+    });
+}
+
+function captureImage(){
+  if (width && height) {
+    contextOne.drawImage(video, 0, 0, width, height);
+    saveImage();
+  }
+}
+
+function saveImage(){
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+  data = canvasOne.toDataURL('image/png');
+  localStorage.setItem(key, data);
+}
---
+++ html5포폴_new_design/sw-test-newlyDeveloping/canvas.js
@@ -0,0 +1,95 @@
+// <REFERENCES>
+// https://www.youtube.com/results?search_query=Learn+HTML5+Canvas+By+Creating+A+Drawing+App+%7C+HTML+Canvas+Tutorial
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/mousedown_event
+
+// <NOTES>
+// click: press+release combined
+// mousedown: fired the moment the button is initially pressed.
+
+// <VARIABLES>
+let canvas = document.getElementById('canvasOne');
+let context = canvas.getContext('2d');
+let painting = false;
+
+let frame = document.getElementById('frame');
+let frameContext = frame.getContext('2d');
+let photoFrameImage = new Image();
+photoFrameImage.src = '../images/photoFrameImage.png'; //https://toppng.com/old-frame-border-png-old-photo-frames-PNG-free-PNG-Images_167585
+
+photoFrameImage.addEventListener('load', function() {
+  // execute drawImage statements here
+  frameContext.drawImage(photoFrameImage, 0, 0);
+}, false);
+
+let blackButton = document.getElementById('blackButton');
+let redButton = document.getElementById('redButton');
+let yellowButton = document.getElementById('yellowButton');
+let saveButton = document.getElementById('saveButton');
+
+loadImage();
+
+blackButton.addEventListener('click', changePenColorToBlack);
+redButton.addEventListener('click', changePenColorToRed);
+yellowButton.addEventListener('click', changePenColorToYellow);
+saveButton.addEventListener('click', savePhotoWithDrawing);
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', finishPosition);
+canvas.addEventListener('mousemove', draw);
+
+function startPosition(event) {
+  painting = true;
+  draw(event); //for drawing dots
+}
+  
+function finishPosition() {
+  painting = false;
+  context.beginPath(); //to start new lines after one another
+}
+// beginPath->moveTo->lineTo->stroke
+function changePenColorToBlack(){
+  context.strokeStyle = "black";
+}
+
+function changePenColorToYellow(){
+  context.strokeStyle = "yellow";
+}
+
+function changePenColorToRed(){
+  context.strokeStyle = "red";
+}
+
+function draw(event) {  
+  if (!painting) return;    
+  context.lineWidth = 2; //drawing pen width
+  context.lineCap = 'round';
+  // context.strokeStyle = "black";
+  context.lineTo(event.offsetX-40, event.offsetY-45);  
+  context.stroke();  
+  context.beginPath(); //starts a new path by emptying the list of sub-paths.  
+  context.moveTo(event.offsetX-40, event.offsetY-45);
+  // console.log(event.offsetX+" "+event.offsetY) //for testing coordinates
+}
+
+function savePhotoWithDrawing(){
+  saveImage();
+}
+
+function saveImage(){
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+  let key = "photoKey"; 
+  data = canvasOne.toDataURL('image/png');
+  localStorage.setItem(key, data);
+}
+
+function loadImage(){
+  let retrievingData = localStorage.getItem(key);
+  console.log("retrievingData: " + retrievingData);
+  
+  let img = new Image();
+  img.onload = function(){
+    context.drawImage(img, 0, 0);
+  }
+  // console.log("changed to canvas");
+  img.src = retrievingData;
+}
