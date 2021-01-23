80184723e33e71d8f86e6ee62d5619962083e874
7inug1 <7inug1@gmail.com>
2021-01-23 18:05:39 +0900

    multi-tag functionality added

--- note-app/src/app.jsx
+++ note-app/src/app.jsx
@@ -11,14 +11,18 @@ class app extends Component {
       newNoteContent: "",
 
       notes: [
-        {"title": "How to make a soup", "tag": "recipe", "content": "Put the powder into the pot and boil it."},
-        {"title": "How to study", "tag": "lifehack", "content": "Just do it."},
-        {"title": "How to make a katsu", "tag": "recipe", "content": "Fry chicken or pork."},
-        {"title": "How to save money", "tag": "lifehack", "content": "Just save it."},
-        {"title": "What is life?", "tag": "philosophy", "content": "Life is something that has no meaning itself. You make of your own."}
+        {"title": "How to make a soup", "tag": ["recipe", "soup"], "content": "Put the powder into the pot and boil it."},
+        {"title": "계란밥 만드는 법", "tag": ["recipe", "lifehack"], "content": "계란에 밥 비비기"},
+        {"title": "How to study", "tag": ["lifehack"], "content": "Just do it."},
+        {"title": "How to make a katsu", "tag": ["recipe"], "content": "Fry chicken or pork."},
+        {"title": "How to save money", "tag": ["lifehack"], "content": "Just save it."},
+        {"title": "What is life?", "tag": ["philosophy"], "content": "Life is something that has no meaning itself. You make of your own."}
       ],
+
       filteringTag: undefined,
+      
       filteredNotes: [],
+      
       unduplicatedTagsArray: [],
       currentlyClickedFilter: false
       
@@ -30,61 +34,64 @@ class app extends Component {
     this.submitNewNote = this.submitNewNote.bind(this);
     this.addTags = this.addTags.bind(this);
     this.removeTags = this.removeTags.bind(this);
-    
-    
   }
   
   componentDidMount(){
-    console.log("componentDidMount")
-    const tempTagArray = this.state.notes.map((note)=>note.tag)
-
+    // console.log("componentDidMount")
+    const totalTagsArray = this.state.notes.map((note)=>note.tag)
+    const totalTagsArrayFlat = totalTagsArray.flat()
     this.setState({
-      unduplicatedTagsArray: [...new Set(tempTagArray)],
-      filteredNote: this.state.notes
+      unduplicatedTagsArray: [...new Set(totalTagsArrayFlat)],
     })
-    this.getNotesByTag();
+    this.getNotesByTag(); // getting entire notes by not passing any parameter
   }
 
   getNotesByTag(tag){
-    console.log("getNotesByTag")
-
+    // console.log("getNotesByTag")
+    // 1. get entire notes
     if(tag===undefined){
       this.setState({
-        filteredNotes: this.state.notes
+        filteredNotes: this.state.notes // showing entire notes at the beginning
       })
     }
+    
+    // 2. get specific notes by 'filteringTag'
     else{
-      const tempNotesArray = [];
+      const tempFilteredNotesArray = [];
 
       this.setState({
         filteringTag: tag
       })
 
       for(let i=0; i<this.state.notes.length; i++){
-        if(this.state.notes[i].tag === tag){
-          tempNotesArray.push(this.state.notes[i]);
+        for(let j=0; j<this.state.notes[i].tag.length; j++){
+          if(this.state.notes[i].tag[j] === tag){
+            tempFilteredNotesArray.push(this.state.notes[i]);
+          }
         }
       }
 
       this.setState({
-        filteredNotes: tempNotesArray
+        filteredNotes: tempFilteredNotesArray
       })
     }
   }
 
   componentDidUpdate(prevProps, prevState){
-    console.log("componentDidUpdate")
-
+    // console.log("componentDidUpdate")
+    // 1. re-spread 'unduplicatedTagsArray' buttons
     if(prevState.notes !== this.state.notes){
-      const tempTagArray = this.state.notes.map((note)=>note.tag)
-      console.log("tempTagArray: " + tempTagArray)
-      this.setState({
-        unduplicatedTagsArray: [...new Set(tempTagArray)]
-      })
+      const tempTagArray = this.state.notes.map((note)=>note.tag) // 1. get all notes' tag
+      const tempTagArrayFlat = tempTagArray.flat() // 2. make #1 flat
+      const tempTagArrayFlatUnduplicated = [...new Set(tempTagArrayFlat)] // 3. get #2 unduplicated
 
+      this.setState({
+        unduplicatedTagsArray: tempTagArrayFlatUnduplicated
+      })
+      
+      // 2. re-spread the notes
       this.getNotesByTag(this.state.filteringTag);
     }
-    console.log("componentDidUpdate finished")
   }
 
   handleNewNoteTitleChange(event){
@@ -101,31 +108,30 @@ class app extends Component {
 
   addTags(event){
     event.preventDefault();
-    console.log("addTags -> newNoteTags: " + this.state.newNoteTags)
-    // if(event.key==="Shift"){
-    //   console.log("Shift")
-    // }
+
     if(event.key==="Shift"&&event.target.value){
-      const tempArray = this.state.newNoteTags
-      tempArray.push(event.target.value)
-      console.log("tempArray: " + tempArray)
-      for(let i = 0; i< tempArray.length; i++){
-        console.log("tempArray[i]: " + tempArray[i])
-      }
+      console.log("added")
+      const tempNewNoteTagsArray = this.state.newNoteTags
+      tempNewNoteTagsArray.push(event.target.value)
+
       this.setState({
-        newNoteTags: tempArray
+        newNoteTags: tempNewNoteTagsArray
       });  
+      console.log("newNoteTags: " + "["+this.state.newNoteTags+"]")
       event.target.value = "";
     } 
   }
 
-  removeTags(key){
-    console.log("removeTags")
-    let tempTagArray = this.state.newNoteTags.splice(key, 1)
-    // console.log("removeTags -> tempTagArray: " + tempTagArray)
+  removeTags(event, key){
+    event.preventDefault();
+
+    let tempNewNoteTagsArray = this.state.newNoteTags
+    tempNewNoteTagsArray.splice(key, 1)
+
     this.setState({
-      newNoteTags: tempTagArray
+      newNoteTags: tempNewNoteTagsArray
     })
+    console.log("newNoteTags: " + "["+this.state.newNoteTags+"]")
   }
 
   handleNewNoteContentChange(event){
@@ -137,19 +143,22 @@ class app extends Component {
   submitNewNote(event){
     event.preventDefault();
     // console.log(this.state.newNoteTitle);
-    console.log(this.state.newNoteTags);
+    // console.log(this.state.newNoteTags);
     // console.log(this.state.newNoteContent);
     // if(this.state.newNoteTitle==="" || !this.state.newNoteTags==="" || !this.state.newNoteContent===""){
     //   alert("Please fill out the form for all the sections.")
     // }
     // else{
+
       this.setState({
         notes: [...this.state.notes, { "title": this.state.newNoteTitle, "tag": this.state.newNoteTags, "content": this.state.newNoteContent}],
         newNoteTitle: "",
-        newNoteTags: "",
+        newNoteTags: [],
         newNoteContent: ""
       });
       event.target.reset();
+
+      console.log(this.state.notes)
     // }
   }
 
@@ -174,7 +183,7 @@ class app extends Component {
                 <li key={key}>
                   <h1>key: {key}</h1>
                   <span>{newNoteTag}</span>
-                  <button onClick={()=>this.removeTags(key)}>X</button>
+                  <button onClick={(event)=>this.removeTags(event, key)}>X</button>
                 </li>
               )}
             </ul>
@@ -201,11 +210,22 @@ class app extends Component {
 
         {/* 3. Note section */}
         <h1>Filtered Notes</h1>
-        {this.state.filteredNotes.map((filteredNote)=>
+        {this.state.filteredNotes.map((filteredNote, key)=>
           <div className="note-individual">
-            <h3 key={filteredNote.title}> Title: {filteredNote.title} </h3>
-            <h3 key={filteredNote.tag}> Tag: {filteredNote.tag} </h3>
-            <h3 key={filteredNote.content}> Content: {filteredNote.content} </h3>
+            <h3 key={filteredNote.title}> 
+              Title: {filteredNote.title} 
+            </h3>
+
+            <h3 key={filteredNote.tag}> 
+              Tag: 
+              {filteredNote.tag.map((tag, key)=>
+                <li key={key}>{key+1}. {tag}</li>
+              )} 
+            </h3>
+
+            <h3 key={filteredNote.content}> 
+              Content: {filteredNote.content} 
+            </h3>
           </div>
         )}
       </>
