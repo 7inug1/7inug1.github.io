33276f314474b6fd1cb7dd9d9184ecd2cd29ebed
7inug1 <7inug1@gmail.com>
2021-03-13 10:33:05 +0900

    feat(update): responsive message and footer update

--- doraemonJSminiGamePortfolio/doraemonJSminiGame.js
+++ doraemonJSminiGamePortfolio/doraemonJSminiGame.js
@@ -1,9 +1,3 @@
-// -Name : Jinwook Shin
-//
-// -From : Langara College, Computer Studies
-//
-// -Last Date Modified : 4/19/2020 (Sun)
-//
 // -Instruction : 
 // Use arrowleft and arrowright keyboards to move the character around to catch the
 // snack dropping from the top. If you achieve the designated score, you win the game.
@@ -19,17 +13,14 @@
 // width:600
 // height:400
 
-//Using canvas
 let canvas=document.querySelector("#canvas");
 let ctx=canvas.getContext("2d");
 //------------------------------------------------
-//Global variables
 let score=0;
 let goal=5;
-let timer;
-let time;
+let timer = 0;
+let time = 0;
 //------------------------------------------------
-//Calling image to use
 let background=new Image();
 background.src="./img/room.jpg";
 let doraemon=new Image();//basket
@@ -39,23 +30,20 @@ dorayaki.src='./img/dorayaki.png';
 //------------------------------------------------
 let dora=new Doraemon(-25, 300); //doraemon's starting position
 let yaki=new Dorayaki((Math.floor)(Math.random()*556), -15); //yaki being called at a random position at the top
-
 let arrowLeftButton = document.querySelector('#arrowLeftButton');
 let arrowRightButton = document.querySelector('#arrowRightButton');
 
-
-window.onload = redraw; //Upon loading the page, you redraw
-document.addEventListener("keydown", pressdown); //can move doraemon using arrowleft and arrowright keyboard
+window.onload = redraw; 
+document.addEventListener("keydown", pressdown); 
 document.addEventListener("keyup", pressup);
+
 displayGoal();
-//------------------------------------------------
-//Constructors
-//basket
+
 function Doraemon(x, y){
   this.x=x;
   this.y=y;
 }
-//apple
+
 function Dorayaki(x, y){
   this.x=x;
   this.y=y;
@@ -120,7 +108,6 @@ function pressup(event){
   }
 }
 //------------------------------------------------
-
 function checkCollision(){
   //if(yakileft vs left / yakileft vs right) || yakiright vs left / yakiright vs right
   if( (yaki.x-30>=dora.x)&&(yaki.x<=dora.x+80) || (yaki.x+45-30>=dora.x)&&(yaki.x+45<=dora.x+100) ){
